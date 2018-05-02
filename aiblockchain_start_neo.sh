e3ef5bc49bc845887efa864034a47064af18fc34
decryptoid <decryptoid@gmail.com>
2018-05-02 18:16:27 -0400

    Script

---
+++ aiblockchain_start_neo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo
+echo "***********************************"
+echo "          A.I Blockchain"
+echo "      NEO development server"
+echo "         ai-blockchain.com"
+echo "***********************************"
+echo
+echo  Starting NEO consensys Nodes...
+docker-compose up
+echo
+echo "***********************************"
+echo "          A.I Blockchain"
+echo "      NEO development server"
+echo "         ai-blockchain.com"
+echo "***********************************"
+echo
+echo  STOPPED
+echo
+
